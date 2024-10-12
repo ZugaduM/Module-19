e1f27e26562cc1114be7aaf5b2485d928417d91b
Geldion <zugadum@mail.ru>
2024-10-13 01:15:23 +0300

    Modified urls by task requirements

--- Django/Django/urls.py
+++ Django/Django/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from task1.views import index, second, Third, sign_up_by_django
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', sign_up_by_django),
+    path('index/', index),
+    path('shop/', second),
+    path('stat/', Third.as_view()),
 ]
