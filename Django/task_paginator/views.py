f9ca089896d447b6b845c413c66644eca900c251
Geldion <zugadum@mail.ru>
2024-10-14 22:34:29 +0300

    Add data which transmitted to template

---
+++ Django/task_paginator/views.py
@@ -0,0 +1,17 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render
+from .models import *
+
+
+def main(request):
+    posts = Post.objects.all().order_by('-created_at')
+    paginator = Paginator(posts, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    Authors = Author.objects.all()
+    context = {
+        "Authors": Authors,
+        "page_obj": page_obj,
+    }
+    return render(request, 'paginator.html', context)
